e86673abd1c37d1bae6c570eceefdfe108b50a1e
natikus <natikumy2@gmail.com>
2024-09-17 15:03:49 -0300

    funciona el ouath

--- backend/src/tipos/fastify.ts
+++ backend/src/tipos/fastify.ts
@@ -5,14 +5,17 @@ export interface authenticateFunction {
     (request: FastifyRequest, reply: FastifyReply): Promise<void>;
 }
 
-declare module "fastify" {
-    interface FastifyInstance {
-        authenticate: authenticateFunction;
-    }
-}
+
 
 declare module 'fastify' {
     interface FastifyInstance {
-        googleOAuth2: any; // Puedes usar el tipo correcto si está disponible
+        googleOAuth2: any;
+    }
+}
+
+
+declare module "fastify" {
+    interface FastifyInstance {
+        authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
     }
 }
