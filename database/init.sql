1df384e3f1f4a895a68302124bcc46a180cd18d9
Sopinga <franco.malugani@correo.ucu.edu.uy>
2024-09-20 18:27:27 -0300

    asdg

--- database/init.sql
+++ database/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS personas (
 );
 
 INSERT INTO personas (nombre, nombre2, apellido, email, cedula, rut, contrasena)
-    VALUES ('Pepito', 'Pepe',  'Rodriguez', 'roberto@hotmail.com', '5.440.395-7', 214849650014, crypt('Juancito!8442', gen_salt('bf'))),
+    VALUES ('Franco', 'Pepe',  'Malugani', 'famalugani@gmail.com', '5.440.395-7', 214849650014, crypt('Juancito!8442', gen_salt('bf'))),
            ('Pepita', 'ana', 'perez', 'perez@hotmail.com', '5.479.508-3', 160338520015, crypt('Pepita!8442', gen_salt('bf'))),
            
            ('jorgita','jorga', 'hernandez', 'hernandez@hotmail.com', '3.918.898-2', 160205570011, crypt('Jorgita!8442', gen_salt('bf')));
